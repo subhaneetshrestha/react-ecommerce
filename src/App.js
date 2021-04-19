e20d524047499e2563ad6d93c6e216256b2a650d
Subhaneet Shrestha <subhaneet@DESKTOP-3N8FU0E.localdomain>
2021-04-19 23:22:47 +0545

    added basic route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage.component.jsx';
 
-function App() {
+const App = () => {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path='/' component={ Homepage } />
+      </Switch>
     </div>
   );
 }
